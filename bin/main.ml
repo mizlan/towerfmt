28d6939db2222ff505a58b51b793c8e5a9263dab
Michael Lan <michaellan202@gmail.com>
2025-05-19 11:05:36 -0700

    cli: Parse multiple lines

--- bin/main.ml
+++ bin/main.ml
@@ -2,8 +2,9 @@ open Towerfmt
 
 let () =
   let input = In_channel.(input_lines stdin) in
-  let i = List.hd input in
-  let res = Parse_tower.parse i in
-  match res with
-  | Ok res -> Print_pl.pp (Format.get_std_formatter ()) res
-  | Error e -> print_endline e
+  List.iter
+    (fun i ->
+      match Parse_tower.parse i with
+      | Ok res -> Print_pl.pp (Format.get_std_formatter ()) res
+      | Error e -> print_endline e)
+    input
